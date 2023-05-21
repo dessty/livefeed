8aa60808369021b066b8ac87ccca94565647e3c5
Steve Delor <st.delor@gmail.com>
2023-05-21 16:25:11 -0400

    build(server): express app and backend api integrated with UI
    BREAKING CHANGE: first working prototype

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const DataAccessObject = require('./dataAccessObject');
+const Comment = require('./comment');
+const cors = require('cors'); // added cors to prevent cross origin error
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: '*'
+}));
+
+const dataAccessObject = new DataAccessObject('./database.sqlite3');
+const comment = new Comment(dataAccessObject);
+
+comment.createTable().catch(error => {
+  console.log(`Error: ${JSON.stringify(error)}`);
+});
+
+app.post('/createComment', function(request, response) {
+  const { body } = request;
+  comment.createComment(body).then(result => {
+    console.log("/createComment -", result);
+    response.send(result);
+  });
+});
+
+app.get('/getComment', function(request, response) {
+  const { body } = request;
+  const { id } = body;
+  comment.getComment(id).then(result => {
+    response.send(result);
+  });
+});
+
+app.get('/getComments', function(request, response) {
+  comment.getComments().then(result => {
+    console.log("/getComments -", result);
+    response.send(result);
+  });
+});
+
+app.delete('/deleteComments', function(request, response) {
+  comment.deleteComments().then(result => {
+    response.send(result);
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  const rootDir = __dirname.replace('/server', '');
+  response.sendFile(`${rootDir}/src/index.html`);
+});
